1bfe9cf36a715307bd322e9660116450ba28da35
Trond Nordheim <trond@nordheim.io>
2019-05-08 14:21:39 +0200

    Removing hydra sdk dep

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/tanordheim/hydra-sdk-dep-test
 
 go 1.12
-
-require github.com/ory/hydra/sdk/go/hydra v0.0.0-20190508091557-5eeb162ccef6
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ory/hydra/sdk/go/hydra/client"
 )
 
 func main() {
-	c := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{})
-	fmt.Printf("c=%+v\n", c)
+	fmt.Printf("hello\n")
+	// c := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{})
 }
